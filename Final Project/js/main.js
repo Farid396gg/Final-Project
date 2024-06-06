18be602ea6179b395d045231581d417d17f4bf0c
Farid396gg <166509047+Farid396gg@users.noreply.github.com>
2024-06-07 00:12:31 +0400

    Add files via upload

---
+++ Final Project/js/main.js
@@ -0,0 +1,42 @@
+
+
+document.getElementById('phone').addEventListener('input', function (e) {
+  this.value = this.value.replace(/[^0-9]/g, '');
+});
+
+
+$(function(){
+
+  $('.carousel__inner').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+    ]
+  });
+
+  wow = new WOW(
+    {
+      boxClass: 'wow',      
+      animateClass: 'animate__animated', 
+      offset: 0,          
+      mobile: true,       
+      live: true        
+    }
+  )
+  wow.init();
+
+
+});
